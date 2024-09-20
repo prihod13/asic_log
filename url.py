242e96134e20ed1224734095dfeb2d0186629a09
prihod13 <polovinko-2011@mail.ru>
2024-09-20 15:32:33 +0300

    aisc_full1

--- url.py
+++ url.py
@@ -3,7 +3,7 @@ from requests.auth import HTTPBasicAuth,HTTPDigestAuth
 import requests
 import json
 import time
-
+import urllib.request
 
 def time_pg():
     named_tuple = time.localtime()  # получить struct_time
@@ -13,39 +13,45 @@ def time_pg():
 
 
 
-user ='root'
-password='root'
-hostname='http://192.168.1.20/cgi-bin/stats.cgi'
+def connect_asic():
+    try:
+        user = 'root'
+        password = 'root'
+        hostname = 'http://192.168.1.20/cgi-bin/stats.cgi'
 
-auth = requests.get(hostname, auth=HTTPDigestAuth(user, password))
-print(auth)
-#auths='http://192.168.1.20/cgi-bin/stats.cgi'
+        auth = requests.get(hostname, auth=HTTPDigestAuth(user, password))
+        print(auth)
+        soup = BeautifulSoup(auth.text, 'html.parser')
+        file = "asic_log.json"
+        file_log_ubuntu = "file_log_ubuntu"
+        with open(file, "w+") as f:
+            f.write(str(soup))
+
+        with open(file_log_ubuntu, "a+") as f:
+            f.write(time_pg() + str(soup))
+
+        with open(file, "r") as f:
+            data = json.load(f)
+            for p in data['STATS']:
+                print(p['rate_5s'])
+                print(p['rate_30m'])
+                print(p['rate_avg'])
+                print(p['fan'])
+                print(p['chain'])
+    except ValueError as err:
+        print(err)
 
 
 
-soup = BeautifulSoup(auth.text,'html.parser')
-#print(soup)
 
-
-file= "asic_log.json"
-file_log_ubuntu = "file_log_ubuntu"
-with open(file, "w+") as f:
-    f.write(str(soup))
-
-with open(file_log_ubuntu, "a+") as f:
-    f.write(time_pg() + str(soup))
+if __name__ == "__main__":
+    connect_asic()
 
 
 
 
-with open(file, "r") as f:
-    data = json.load(f)
-    for p in data['STATS']:
-        print(p['rate_5s'])
-        print(p['rate_30m'])
-        print(p['rate_avg'])
-        print(p['fan'])
-        print(p['chain'])
+
+
 
 
 
