a79dda22d9226925d516a4909d238cd3ccd06c41
prihod13 <polovinko-2011@mail.ru>
2024-09-20 15:08:38 +0300

    aisc_full

--- url.py
+++ url.py
@@ -2,6 +2,16 @@ from bs4 import BeautifulSoup
 from requests.auth import HTTPBasicAuth,HTTPDigestAuth
 import requests
 import json
+import time
+
+
+def time_pg():
+    named_tuple = time.localtime()  # получить struct_time
+    time_string = time.strftime("log_time# %H:%M:%S ___ %D ", named_tuple)
+
+    return str(time_string)
+
+
 
 user ='root'
 password='root'
@@ -18,8 +28,14 @@ soup = BeautifulSoup(auth.text,'html.parser')
 
 
 file= "asic_log.json"
+file_log_ubuntu = "file_log_ubuntu"
 with open(file, "w+") as f:
     f.write(str(soup))
+
+with open(file_log_ubuntu, "a+") as f:
+    f.write(time_pg() + str(soup))
+
+
 
 
 with open(file, "r") as f:
