b838644559094117153f409fc7e2d94f8ba74103
prihod13 <70544951+prihod13@users.noreply.github.com>
2024-09-24 13:03:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,16 @@ with open(file, "r") as f:
 
 
 
+import subprocess
 
+URL = "http://192.186.1.20"
 
+comand= "ping 192.168.1.20"
 
+status = subprocess.run(['ping', '-c', '2', '-n', '192.168.1.20'],
+                            stderr=subprocess.PIPE, encoding='utf-8')
+
+#print(status.stderr)
 
 
 
